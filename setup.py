f2a134ed03d93dcb9b08016425370035d7a74609
dchason <davidchason@gmail.com>
2019-10-31 21:02:38 -0400

    setup update versioning

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="active911_python",
-    version="0.0.1",
+    version="0.0.2",
     author="David Chason",
     author_email="davidchason@gmail.com",
     description="Python bindings for active911 API",
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3.0",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
