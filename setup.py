af733aecf261137f3e966f415027ae031458a860
dchason <davidchason@gmail.com>
2019-10-31 20:03:24 -0400

    fixed setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="active911_python",e
+    name="active911_python",
     version="0.0.1",
     author="David Chason",
     author_email="davidchason@gmail.com",
