e55d5cd0287f0ae3ee6b37f5933c402a025e66be
dchason <davidchason@gmail.com>
2019-10-31 20:52:11 -0400

    updated readme

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Python bindings for active911 API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/dc105297/active911_python",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
