723e02efd87cfaa585327195b9308e1e770be3f0
dchason <davidchason@gmail.com>
2019-11-04 19:24:19 -0500

    setup.py update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="active911_python",
-    version="0.0.2",
+    version="0.0.3",
     author="David Chason",
     author_email="davidchason@gmail.com",
     description="Python bindings for active911 API",
